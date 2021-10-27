44ed2fa4a7a89b1c39550cc9471d1f27211040c5
Michael Valdron <michael.valdron@gmail.com>
2021-10-27 14:52:08 -0400

    fixed leaking of the typing module.

--- test_main.py
+++ test_main.py
@@ -1,8 +1,9 @@
 import os
 import unittest
 from unittest import TestCase
+from typing import Tuple
 
-from main import *
+import main
 
 
 class Test(TestCase):
@@ -18,12 +19,12 @@ class Test(TestCase):
         invalid = [{"student1": ["view_grades", "view_classes"]},
                    {"student2": ["view_grades", "view_classes"]},
                    {"teacher": ["view_grades", "change_grades", "add_grades", "delete_grades", "view_classes"]}]
-        self.assertTrue(validate_json(valid))
-        self.assertFalse(validate_json(invalid))
+        self.assertTrue(main.validate_json(valid))
+        self.assertFalse(main.validate_json(invalid))
 
     def test_parse_json(self):
-        result_one = parse_json(self._json_files[0])
-        result_two = parse_json(self._json_files[1])
+        result_one = main.parse_json(self._json_files[0])
+        result_two = main.parse_json(self._json_files[1])
         self.assertListEqual(["student1", "student2", "student3", "teacher"], list(result_one.keys()))
         self.assertListEqual(["view_grades", "change_grades", "add_grades", "delete_grades", "view_classes"],
                              result_one["teacher"])
@@ -33,20 +34,20 @@ class Test(TestCase):
 
     def test_update_lists_to_sets(self):
         records = {"student1": ["view_grades", "view_classes"]}
-        result = next(update_lists_to_sets(records))
+        result = next(main.update_lists_to_sets(records))
         self.assertEqual("student1", result[0])
         self.assertTrue("view_grades" in result[1])
 
     def test_create_csv(self):
         try:
             headers = ['a', 'b', 'c']
-            result = create_csv(self._csv_file, headers)
+            result = main.create_csv(self._csv_file, headers)
             self.assertTrue(result)
             with open(self._csv_file, 'r') as fp:
                 self.assertEqual("a,b,c\n", fp.read())
         finally:
-            if os.path.exists(self._csv_file):
-                os.remove(self._csv_file)
+            if main.os.path.exists(self._csv_file):
+                main.os.remove(self._csv_file)
 
     def test_write_to_csv(self):
         try:
@@ -56,7 +57,7 @@ class Test(TestCase):
             records = {"student1": ["view_grades", "view_classes"],
                        "student2": ["view_grades", "view_classes"],
                        "teacher": ["view_grades", "change_grades", "add_grades", "delete_grades", "view_classes"]}
-            result = write_to_csv(self._csv_file, headers, records)
+            result = main.write_to_csv(self._csv_file, headers, records)
             self.assertTrue(result)
             with open(self._csv_file, 'r') as fp:
                 self.assertEqual("person,view_grades,change_grades,add_grades,"
@@ -64,14 +65,14 @@ class Test(TestCase):
                                  "delete_classes\nstudent1,0,1,0,0,0,1,0,0,0\n"
                                  "student2,0,1,0,0,0,1,0,0,0\nteacher,0,1,1,1,1,1,0,0,0\n", fp.read())
         finally:
-            if os.path.exists(self._csv_file):
-                os.remove(self._csv_file)
+            if main.os.path.exists(self._csv_file):
+                main.os.remove(self._csv_file)
 
     def test_main(self):
         try:
-            self.assertEqual(1, main(["main.py"]))
-            self.assertEqual(1, main(["main.py", self._json_files[0]]))
-            self.assertEqual(0, main(["main.py", self._json_files[0], self._csv_file]))
+            self.assertEqual(1, main.main(["main.py"]))
+            self.assertEqual(1, main.main(["main.py", self._json_files[0]]))
+            self.assertEqual(0, main.main(["main.py", self._json_files[0], self._csv_file]))
             with open(self._csv_file, 'r') as fp:
                 self.assertEqual("person,view_grades,change_grades,add_grades,"
                                  "delete_grades,view_classes,change_classes,add_classes,"
@@ -79,8 +80,8 @@ class Test(TestCase):
                                  "student2,0,1,0,0,0,1,0,0,0\nstudent3,0,1,0,0,0,1,0,0,0\n"
                                  "teacher,0,1,1,1,1,1,0,0,0\n", fp.read())
         finally:
-            if os.path.exists(self._csv_file):
-                os.remove(self._csv_file)
+            if main.os.path.exists(self._csv_file):
+                main.os.remove(self._csv_file)
 
 
 if __name__ == '__main__':
